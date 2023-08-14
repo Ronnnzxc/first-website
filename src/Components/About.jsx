456c9133911b1336bcaf310afea99de1781b7196
Ronnnzxc <141236849+Ronnnzxc@users.noreply.github.com>
2023-08-14 14:01:53 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science Student who enjoy learning and testing ML/AI and CyberSecurity stuff and loves to hang around.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Web Developer",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
+  "Git Version Control",
+  "Pentesting",
   "Mobile user interfaces",
-  "Graphic design",
+  "Intermediate in Java",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems and training data using machine learning in order to amke an AI. I continually create a tech that will be convenient, new, and accesible to all.";
 
 const About = () => {
   return (
